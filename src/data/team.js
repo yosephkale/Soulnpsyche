967e7b8fd3ad078721695d767dca7ecf889d2c06
Kapil Dogiyal <kapil.dogiyal@example.com>
2026-03-17 15:52:52 +0530

    Completed tasks

--- src/data/team.js
+++ src/data/team.js
@@ -1,29 +1,34 @@
+import Team1 from "../assets/Team1.webp";
+import Team2 from "../assets/Team2.webp";
+import Team3 from "../assets/Team3.webp";
+import Team4 from "../assets/Team4.webp";
+
 export const teamMembers = [
     {
         name: "Dr. Yoseph Kale",
         designation: "MBBS, DPM",
-        image: "https://drive.google.com/uc?export=view&id=1CR6_3uqQ_qxIdvVSxB3Qon0PrAzzKXv2",
+        image: Team1,
         story:
             "I am a Psychiatrist, certified in Sexual Medicine, and a member of leading global psychiatric associations. Co-author of India's first paper on Frog Sweat Addiction. I love music, writing, travel, and ice creams.",
     },
     {
         name: "Kamalpreet Kaur",
         designation: "Psychologist",
-        image: "https://drive.google.com/uc?export=view&id=1UFQmWEH15ktLWcUy_TMzEQG6DUU-cjo9",
+        image: Team2,
         story:
             "I am a Psychologist with expertise in counselling, emotional intelligence, and mental wellness. Certified by APA in Hoarding Disorder and Nutrition's Role in Mental Health. A creative mind who finds joy in art, music, and design.",
     },
     {
         name: "Dr. Rupesh G. E",
         designation: "MBBS, MD - Psychiatry",
-        image: "https://drive.google.com/uc?export=view&id=1VGa9UT1TE60bVt_HLKOIaBClNkOIg2hI",
+        image: Team3,
         story:
             "I am a Child Psychiatrist and fellow of top psychiatric associations, with research on Internet Addiction, Parenting, and Elderly Mental Health. Passionate about mentoring and exploring human behaviour.",
     },
     {
         name: "Rasika Enagandula",
-        designation: "Certified Yoga Teacher",
-        image: "https://drive.google.com/uc?export=view&id=10X4sh-J96crQUl9PKbSVi-bJvnVD4Slz",
+        designation: "Certified Yoga Teacher and Sound Healer",
+        image: Team4,
         story:
             "I am the Founder of Nirvana Yogshala, Patiala. Certified in Hatha, Ashtanga, and Sivananda Yoga with 5+ years of teaching. National gold medalist in swimming and yoga enthusiast who loves reading, travel, and quiet moments in nature.",
     },
